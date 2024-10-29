d00631f671c1c052e3c95ca811eb896066df1905
Tzu-An Sheng <tasheng@mit.edu>
2024-10-29 18:50:19 -0400

    Adapt for HF

--- ratecheck.py
+++ ratecheck.py
@@ -1,51 +1,37 @@
-from util.oms import omsapi
+# from oms.omstools.util.oms import get_hltlist_by_run
+from util.oms import omsapi, get_hltlist_by_run
 import util.utility as u
 import argparse
 import util.oms as oms
+
 from datetime import datetime, timedelta
+import pytz
 
+import numpy as np
+import pygsheets
 
-# fout = open("rate_monitoring.csv", "w")
+def find_matching_strings(strings, substring):
+    return [s for s in strings if substring in s]
 
-data_taking = 'pp'
-data_taking = 'pbpb'
-
-if data_taking == 'pp':
-    hlt_paths = [
-        "HLT_AK4CaloJet60_v5",
-        "HLT_AK4CaloJet70_v5",
-    ]
-    l1_paths = [
-        "L1_ZeroBias",
-        "L1_SingleJet35",
-        "L1_SingleJet60",
-    ]
-else:
-    hlt_paths = [
-        "HLT_HIL1Centrality30_50_v6",
-        "HLT_HIPuAK4CaloJet100Eta5p1_v13"
-    ]
-    l1_paths = [
-        "L1_ZeroBias",
-        "L1_Centrality_30_50_BptxAND",
-        "L1_SingleJet60_BptxAND"
-    ]
-
-def get_rate_by_runls_range(run, ls = None, category = "hlt"):
+def get_rate_by_runls_range(name, run, ls = None, category = "hlt"):
     if "hlt" in category:
+        name_filter = "path_name"
         if not ls:
             q = omsapi.query("hltpathinfo")
         else:
             q = omsapi.query("hltpathrates")
     else:
+        name_filter = "name"
         q = omsapi.query("l1algorithmtriggers")
-        q.set_verbose(False)
-        q.set_validation(False)
-        q.filter("run_number", run)
+    q.set_verbose(False)
+    q.set_validation(False)
+    q.filter(name_filter, name)\
+     .filter("run_number", run)
     if not ls:
         if "hlt" not in category:
             q.custom("group[granularity]", "run")
     else:
+        q.custom("group[granularity]", "lumisection")
         q.filter("first_lumisection_number",ls[0],"GE")\
          .filter("last_lumisection_number",ls[1],"LE")
 
@@ -56,19 +42,56 @@ def get_rate_by_runls_range(run, ls = None, category = "hlt"):
         q.paginate(page = ipage, per_page = 100)
         qjson = q.data().json()
         data = qjson["data"]
+        # print(data)
         datas.extend(data)
         if qjson["links"]["next"] is None:
             break;
         ipage = ipage+1
-        u.progressbars_summary(ipage - 1)
+    u.progressbars_summary(ipage - 1)
     return datas
+
+# fout = open("rate_monitoring.csv", "w")
+
+data_taking = 'pp'
+# data_taking = 'pbpb'
+
+if data_taking == 'pp':
+    hlt_paths = [
+        "HLT_AK4CaloJet60",
+        "HLT_AK4CaloJet70",
+    ]
+    l1_paths = [
+        "L1_ZeroBias",
+        "L1_SingleJet35",
+        "L1_SingleJet60",
+    ]
+else:
+    hlt_paths = [
+        "HLT_HIL1Centrality30_50",
+        "HLT_HIPuAK4CaloJet100Eta5p1"
+    ]
+    l1_paths = [
+        "L1_ZeroBias",
+        "L1_Centrality_30_50_BptxAND",
+        "L1_SingleJet60_BptxAND"
+    ]
+
 
 
 if __name__ == "__main__":
+    # Authorize pygsheets with the credentials file
+    gc = pygsheets.authorize(outh_file='/home/tasheng/hlt/oms/omstools/credentials.json')
+    # Open the Google Sheet by its URL
+    spreadsheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1nkBI4aMjDM7XmFUuymKpOZGBpl5hy9gv--ZvRq_-Dz4/edit")
+
+    worksheet = spreadsheet.sheet1  # Select the first worksheet (sheet)
+
+    # parse arg
     parser = argparse.ArgumentParser(description = 'Print HLT counts in given lumi ranges of runs')
     parser.add_argument('--pathnames', required = False, help = 'e.g. HLT_ZeroBias_v8,HLT_PPRefL1SingleMu7_v1')
-    parser.add_argument('--outcsv', required = False, help = 'Optional csv output file')
     parser.add_argument('--timerange', help = '(option 3) <start_time>,<end_time>')
+    parser.add_argument('--l1preps', required = False, help = 'Optional store L1 pre PS rate instead of post DT rate', action = "store_true")
+    parser.add_argument('--count', required = False, help = 'Optional store count instead of rate', action = "store_true")
     args = parser.parse_args()
 
     # specify time range in cmd argument
@@ -77,8 +100,11 @@ if __name__ == "__main__":
         timebs = args.timerange.split(",")
     # otherwise use 1 hr from now
     else:
-        # Get the current time
-        now = datetime.now()
+        # Define Geneva timezone
+        geneva_tz = pytz.timezone("Europe/Zurich")
+
+        # Current time in Geneva timezone
+        now = datetime.now(geneva_tz)
 
         # Get the time an hour earlier
         one_hour_earlier = now - timedelta(hours=1)
@@ -98,126 +124,109 @@ if __name__ == "__main__":
     print(runlumi, end = "")
     print("\033[0m")
 
+    rls_list = sorted(runlumi.items())
+    # test time range
+    # rls_list = [['387528', [[284, 286]]]]
 
-rls_list = sorted(runlumi.items())
-for rls in rls_list:
-    print(rls)
-    print('l1')
-    print(rls, type(rls))
-    l1s = get_rate_by_runls_range(rls[0], rls[1][0], "l1")
-    print('hlt')
-    hlts = get_rate_by_runls_range(rls[0], rls[1][0], "hlt")
-exit(1)
+    lumi_values = {}
+    bunch_values = {}
+    # Lumisection info
+    for rls in rls_list:
+        run = rls[0]
+        q = omsapi.query("lumisections")
+        q.paginate(per_page = 1000)
+        q.set_verbose(False)
+        # q.custom("group[granularity]", "lumisection")
+        q.filter("run_number", rls[0])
+        q.filter("first_lumisection_number",rls[1][0][0],"GE")
+        q.filter("last_lumisection_number",rls[1][0][1],"LE")
+        lumi_data = q.data().json()["data"]
+        lumi_values[run] = np.average(
+            [l["attributes"]["init_lumi"] for l in lumi_data] +
+            [l["attributes"]["end_lumi"] for l in lumi_data]
+        )
+        fill = lumi_data[0]["attributes"]["fill_number"]
+        q_fill = omsapi.query("fills")
+        q_fill.filter("fill_number", fill)
+        fill_data = q_fill.data().json()["data"]
+        bunch_values[run] = fill_data[0]["attributes"]["bunches_colliding"]
 
-counts = {}
-maxlen = 0
-with open(outputfile, 'w') as f:
-    print("HLT Path, Counts", file = f)
+    key_var = "rate"
+    if args.count:
+        key_var = "counter"
+        print("count", end = "")
+    else:
+        print("rate", end = "")
+
+    key_l1 = "post_dt_" + key_var
+    if args.l1preps:
+        key_l1 = "pre_dt_before_prescale_" + key_var
+        print("Pre-DT before PS", end = "")
+    else:
+        print("Post-DT after PS", end = "")
+
+
+    rate_results={};
+    maxlen = 0
+
+    # L1 and HLT rate
+    for rls in rls_list:
+        # assign hlt versions from the current run
+        hltlist = get_hltlist_by_run(rls[0])
+        for i, hltpath in enumerate(hlt_paths):
+            hlt_paths[i] = find_matching_strings(hltlist, hltpath)[0]
+
+
+    # query both pre-PS and post-PS rate
+    extended_l1_paths = [st for path in l1_paths for st in (path, path + " before PS")]
+    pathnames = hlt_paths + extended_l1_paths
     for p in pathnames:
-        totalcount = getcount(runlumi, p)
-        print(p + ", " + f'{totalcount}', file = f)
-        counts[p] = totalcount
+        rate_results[p] = {}
         if len(p) > maxlen: maxlen = len(p)
 
-nl = 21 + maxlen
-print('-' * nl)
-print('| {:<{width}} |{:>15} |'.format("HLT Path", "Count", width = maxlen))
-print('-' * nl)
-for p in counts:
-    print('| {:<{width}} |{:>15} |'.format(p, counts[p], width = maxlen))
-print('-' * nl)
-print()
-exit(1)
 
-ls_starts = [45,80,108] # 0 if we want to start at stable beam.
-ls_num_to_avgs = [20,20,20] # How many lumisections to be averaged.
+    l1s = []
+    hlts = []
+    for rls in rls_list:
+        run = rls[0]
+        for path in pathnames:
+            rate_results[path][run] = np.zeros(rls[1][0][1] - rls[1][0][0])
 
-runnums = [375252,375256,375259] # Run numbers to be used
+        for l1seed in l1_paths:
+            query_lumis = get_rate_by_runls_range(l1seed, rls[0], rls[1][0], "l1")
+            # print(query_lumis)
+
+            key_l1 = "pre_dt_before_prescale_" + key_var
+            rate_results[l1seed + " before PS"][run] = np.average([
+                l["attributes"][key_l1] for l in query_lumis])
+            key_l1 = "post_dt_" + key_var
+            rate_results[l1seed][run] = np.average([
+                l["attributes"][key_l1] for l in query_lumis])
+
+        for hltpath in hlt_paths:
+            print(hltpath)
+            query_lumis = get_rate_by_runls_range(hltpath, rls[0], rls[1][0], "hlt")
+            rate_results[hltpath][run] = np.average([
+                rate["attributes"][key_var] for rate in query_lumis])
+
+        print(rate_results)
 
 
-path_txt = "Run,"
+        slope_35 = 32215.30085
+        intercept_35 = 21.52018581
+        slope_60 = 3706.827046
+        intercept_60 = 2.478517322
 
-for path in l1_paths:
-	path_txt += path
-	path_txt += ","
+        est_35 = lambda lum: slope_35 * lum + intercept_35
+        est_60 = lambda lum: slope_60 * lum + intercept_60
 
-for path in hlt_paths:
-	path_txt += path
-	path_txt += ","
-
-fout.write(path_txt + '\n')
-
-q_lumi = omsapi.query("lumisections")
-q_lumi.paginate(per_page = 3000)
-q_lumi.set_verbose(False)
-
-q_l1 = omsapi.query("l1algorithmtriggers")
-q_l1.paginate(per_page = 3000)
-q_l1.set_verbose(False)
-
-q_hlt = omsapi.query("hltpathrates")
-q_hlt.paginate(per_page = 3000)
-q_hlt.set_verbose(False)
-
-r_id = 0
-
-for runnum in runnums:
-	print('run:',runnum)
-
-	q_lumi.clear_filter()
-	q_lumi.filter("run_number", runnum).filter("beams_stable", 'true')
-	ls_stable_start = q_lumi.data().json()["data"][0]['attributes']['lumisection_number']
-	#ls_start=1
-	#https://cmsoms.cern.ch/agg/api/v1/runs/373710/lumisections?filter[beams_stable]=true
-
-	ls_start = ls_starts[r_id]
-	ls_num_to_avg = ls_num_to_avgs[r_id]
-
-	print('Stable beam start at lumisection',ls_stable_start)
-	if(ls_start==0):
-		ls_start = ls_stable_start
-
-	print('We start at lumisection',ls_start)
-	print('Number of lumisections used to average:',ls_num_to_avg)
-
-	fout.write(str(runnum) + " (LS " + str(ls_start) + "-"  + str(ls_start+ls_num_to_avg-1) + "),")
-
-	for l1_path in l1_paths:
-		q_l1.clear_filter()
-		q_l1.filter("run_number", runnum).filter("name", l1_path).filter("first_lumisection_number",ls_start,"GE").filter("last_lumisection_number",ls_start+ls_num_to_avg,"LE")
-
-		#https://cmsoms.cern.ch/agg/api/v1/l1algorithmtriggers?filter[run_number][EQ]=373710&&filter[name][EQ]=L1_ZeroBias&&filter[first_lumisection_number][GE]=7&&filter[last_lumisection_number][LE]=27
-
-		data=q_l1.data().json()["data"]
-		rate = 0
-		for i in range(ls_num_to_avg):
-			if "MinimumBias" in l1_path:
-				rate += data[i]['attributes']['pre_dt_before_prescale_rate']
-			else:
-				rate += data[i]['attributes']['post_dt_rate']
-
-		rate/=ls_num_to_avg
-		print(l1_path,"rate:",rate)
-
-		fout.write(str(rate) + ',')
-
-	for hlt_path in hlt_paths:
-		q_hlt.clear_filter()
-		q_hlt.filter("run_number", runnum).filter("path_name", hlt_path).filter("first_lumisection_number",ls_start,"GE").filter("last_lumisection_number",ls_start+ls_num_to_avg,"LE")
-
-		#https://cmsoms.cern.ch/agg/api/v1/hltpathrates?filter[run_number][EQ]=373345&&filter[path_name][EQ]=HLT_PPRefGEDPhoton10_v1&&filter[first_lumisection_number][GE]=7&&filter[last_lumisection_number][LE]=27
-
-		data=q_hlt.data().json()["data"]
-		rate = 0
-		for i in range(ls_num_to_avg):
-			rate += data[i]['attributes']['rate']
-
-		rate/=ls_num_to_avg
-		print(hlt_path,"rate:",rate)
-
-		fout.write(str(rate)+ ',')
-
-	fout.write('\n')
-	r_id += 1
-
-print('Done')
+        ## |time start|time end|run|lumi start| lumi end| bunch | ave lumi|
+        row = ["time start","time end","run","lumi start"," lumi end", "bunch", "ave lumi"]
+        row += pathnames
+        row += ['est. jet35', 'est. jet60']
+        worksheet.append_table(values=row)
+        row = [timebs[0], timebs[1], run, rls[1][0][0], rls[1][0][1],
+               bunch_values[run], lumi_values[run]]
+        row += [f"{rate_results[path][run]:.3f}" for path in pathnames]
+        row += [est_35(lumi_values[run]), est_60(lumi_values[run])]
+        worksheet.append_table(values=row)
