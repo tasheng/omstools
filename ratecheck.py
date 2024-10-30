e5d722cbedc6caec0ba1bbf49688f9940baa3a94
Tzu-An Sheng <tasheng@mit.edu>
2024-10-30 02:47:02 -0400

    rate ratios

--- ratecheck.py
+++ ratecheck.py
@@ -220,10 +220,12 @@ if __name__ == "__main__":
         ## |time start|time end|run|lumi start| lumi end| bunch | ave lumi|
         row = ["time start","time end","run","lumi start"," lumi end", "bunch", "ave lumi"]
         row += pathnames
-        row += ['est. jet35', 'est. jet60']
+        row += ['est. jet35', 'est. jet60', 'ratio jet35', 'ratio jet60']
         worksheet.append_table(values=row)
         row = [timebs[0], timebs[1], run, rls[1][0][0], rls[1][0][1],
                bunch_values[run], lumi_values[run]]
         row += [f"{rate_results[path][run]:.3f}" for path in pathnames]
         row += [est_35(lumi_values[run]), est_60(lumi_values[run])]
+        row += [ f"{rate_results['L1_SingleJet35 before PS'][run] / est_35(lumi_values[run]):.3f}" ]
+        row += [ f"{rate_results['L1_SingleJet60 before PS'][run] / est_60(lumi_values[run]):.3f}" ]
         worksheet.append_table(values=row)
